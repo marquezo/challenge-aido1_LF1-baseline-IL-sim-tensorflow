2c8db7e43495a8a16d52dc1b2273eef271e3c2c0
Orlando Marquez <orlando.m.bol@gmail.com>
2018-11-12 14:54:40 -0500

    Going back to one-layer resnet

--- model.py
+++ model.py
@@ -5,7 +5,14 @@ from _layers import one_residual
 
 
 class TensorflowModel:
-    def __init__(self, observation_shape, action_shape, graph_location, lr=1e-4, seed=1234):
+    def __init__(self, observation_shape, action_shape, graph_location, lr=1e-4, seed=1234,
+                 gain=1.0,
+                 trim=0.0,
+                 radius=0.0318,
+                 k=27.0,
+                 limit=1.0,
+                 wheel_dist=0.102
+                 ):
         # model definition
         self._observation = None
         self._action = None
@@ -23,11 +30,60 @@ class TensorflowModel:
 
         self._initialize(observation_shape, action_shape, graph_location)
 
+        # Should be adjusted so that the effective speed of the robot is 0.2 m/s
+        self.gain = gain
+
+        # Directional trim adjustment
+        self.trim = trim
+
+        # Wheel radius
+        self.radius = radius
+
+        # Motor constant
+        self.k = k
+
+        # Wheel velocity limit
+        self.limit = limit
+
+        self.wheel_dist = wheel_dist
+
     def predict(self, state):
+        #gray_image = cv2.cvtColor(observation, cv2.COLOR_RGB2GRAY)
+        #_, observation = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+
         action = self.tf_session.run(self._computation_graph, feed_dict={
             self._observation: [state],
         })
-        return np.squeeze(action)
+
+        return np.squeeze(action)#self.wrap_action(np.squeeze(action))
+
+    def wrap_action(self, action):
+        vel, angle = action
+
+        # assuming same motor constants k for both motors
+        k_r = self.k
+        k_l = self.k
+
+        # adjusting k by gain and trim
+        k_r_inv = (self.gain + self.trim) / k_r
+        k_l_inv = (self.gain - self.trim) / k_l
+
+        omega_r = (vel + 0.5 * angle * self.wheel_dist) / self.radius
+        omega_l = (vel - 0.5 * angle * self.wheel_dist) / self.radius
+
+        # conversion from motor rotation rate to duty cycle
+        u_r = omega_r * k_r_inv
+        u_l = omega_l * k_l_inv
+
+        # limiting output to limit, which is 1.0 for the duckiebot
+        u_r_limited = max(min(u_r, self.limit), -self.limit)
+        u_l_limited = max(min(u_l, self.limit), -self.limit)
+
+        vels = np.array([u_l_limited, u_r_limited])
+
+        print("Vels:", vels)
+
+        return vels
 
     def train(self, observations, actions):
         _, loss = self.tf_session.run([self._optimization_op, self._loss], feed_dict={
@@ -40,62 +96,75 @@ class TensorflowModel:
         self.tf_saver.save(self.tf_session, self.tf_checkpoint)
 
     def computation_graph(self):
-        output = tf.layers.conv2d(self._preprocessed_state, filters=64, kernel_size=3, strides=1, padding='same',
-                              kernel_initializer=tf.keras.initializers.he_normal(seed=self.seed),
-                              kernel_regularizer=tf.keras.regularizers.l2(1e-04))
-        output = tf.layers.batch_normalization(output)
-        output = tf.nn.relu(output)
-
-        output = tf.layers.conv2d(output, filters=64, kernel_size=3, strides=1, padding='same',
-                              kernel_initializer=tf.keras.initializers.he_normal(seed=self.seed),
-                              kernel_regularizer=tf.keras.regularizers.l2(1e-04))
-        output = tf.layers.batch_normalization(output)
-        output = tf.nn.relu(output)
-
-        output = tf.layers.max_pooling2d(output, pool_size=2, strides=2)
-
-        output = tf.layers.conv2d(output, filters=128, kernel_size=3, strides=1, padding='same',
-                              kernel_initializer=tf.keras.initializers.he_normal(seed=self.seed),
-                              kernel_regularizer=tf.keras.regularizers.l2(1e-04))
-        output = tf.layers.batch_normalization(output)
-        output = tf.nn.relu(output)
-
-        output = tf.layers.conv2d(output, filters=128, kernel_size=3, strides=1, padding='same',
-                              kernel_initializer=tf.keras.initializers.he_normal(seed=self.seed),
-                              kernel_regularizer=tf.keras.regularizers.l2(1e-04))
-        output = tf.layers.batch_normalization(output)
-        output = tf.nn.relu(output)
-
-        output = tf.layers.max_pooling2d(output, pool_size=2, strides=2)
-
-        output = tf.layers.conv2d(output, filters=256, kernel_size=3, strides=1, padding='same',
-                              kernel_initializer=tf.keras.initializers.he_normal(seed=self.seed),
-                              kernel_regularizer=tf.keras.regularizers.l2(1e-04))
-        output = tf.layers.batch_normalization(output)
-        output = tf.nn.relu(output)
-
-        output = tf.layers.max_pooling2d(output, pool_size=2, strides=4)
-
-        output = tf.layers.conv2d(output, filters=512, kernel_size=3, strides=1, padding='same',
-                              kernel_initializer=tf.keras.initializers.he_normal(seed=self.seed),
-                              kernel_regularizer=tf.keras.regularizers.l2(1e-04))
-        output = tf.layers.batch_normalization(output)
-        output = tf.nn.relu(output)
-
-        output = tf.layers.max_pooling2d(output, pool_size=2, strides=4)
-
-        output = tf.layers.flatten(output)
-
-        output = tf.layers.dense(output, units=256, activation=tf.nn.relu,
+        model = one_residual(self._preprocessed_state, seed=self.seed)
+        model = tf.layers.dense(model, units=64, activation=tf.nn.relu,
                                 kernel_initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=self.seed),
                                 bias_initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=self.seed))
-        output = tf.layers.dense(output, units=128, activation=tf.nn.relu,
+        model = tf.layers.dense(model, units=32, activation=tf.nn.relu,
                                 kernel_initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=self.seed),
                                 bias_initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=self.seed))
 
-        output = tf.layers.dense(output, self._action.shape[1])
+        model = tf.layers.dense(model, self._action.shape[1])
 
-        return output
+        return model
+
+    # def computation_graph(self):
+    #     output = tf.layers.conv2d(self._preprocessed_state, filters=64, kernel_size=3, strides=1, padding='same',
+    #                           kernel_initializer=tf.keras.initializers.he_normal(seed=self.seed),
+    #                           kernel_regularizer=tf.keras.regularizers.l2(1e-04))
+    #     output = tf.layers.batch_normalization(output)
+    #     output = tf.nn.relu(output)
+    #
+    #     output = tf.layers.conv2d(output, filters=64, kernel_size=3, strides=1, padding='same',
+    #                           kernel_initializer=tf.keras.initializers.he_normal(seed=self.seed),
+    #                           kernel_regularizer=tf.keras.regularizers.l2(1e-04))
+    #     output = tf.layers.batch_normalization(output)
+    #     output = tf.nn.relu(output)
+    #
+    #     output = tf.layers.max_pooling2d(output, pool_size=2, strides=2)
+    #
+    #     output = tf.layers.conv2d(output, filters=128, kernel_size=3, strides=1, padding='same',
+    #                           kernel_initializer=tf.keras.initializers.he_normal(seed=self.seed),
+    #                           kernel_regularizer=tf.keras.regularizers.l2(1e-04))
+    #     output = tf.layers.batch_normalization(output)
+    #     output = tf.nn.relu(output)
+    #
+    #     output = tf.layers.conv2d(output, filters=128, kernel_size=3, strides=1, padding='same',
+    #                           kernel_initializer=tf.keras.initializers.he_normal(seed=self.seed),
+    #                           kernel_regularizer=tf.keras.regularizers.l2(1e-04))
+    #     output = tf.layers.batch_normalization(output)
+    #     output = tf.nn.relu(output)
+    #
+    #     output = tf.layers.max_pooling2d(output, pool_size=2, strides=2)
+    #
+    #     output = tf.layers.conv2d(output, filters=256, kernel_size=3, strides=1, padding='same',
+    #                           kernel_initializer=tf.keras.initializers.he_normal(seed=self.seed),
+    #                           kernel_regularizer=tf.keras.regularizers.l2(1e-04))
+    #     output = tf.layers.batch_normalization(output)
+    #     output = tf.nn.relu(output)
+    #
+    #     output = tf.layers.max_pooling2d(output, pool_size=2, strides=4)
+    #
+    #     output = tf.layers.conv2d(output, filters=512, kernel_size=3, strides=1, padding='same',
+    #                           kernel_initializer=tf.keras.initializers.he_normal(seed=self.seed),
+    #                           kernel_regularizer=tf.keras.regularizers.l2(1e-04))
+    #     output = tf.layers.batch_normalization(output)
+    #     output = tf.nn.relu(output)
+    #
+    #     output = tf.layers.max_pooling2d(output, pool_size=2, strides=4)
+    #
+    #     output = tf.layers.flatten(output)
+    #
+    #     output = tf.layers.dense(output, units=256, activation=tf.nn.relu,
+    #                             kernel_initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=self.seed),
+    #                             bias_initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=self.seed))
+    #     output = tf.layers.dense(output, units=128, activation=tf.nn.relu,
+    #                             kernel_initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=self.seed),
+    #                             bias_initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=self.seed))
+    #
+    #     output = tf.layers.dense(output, self._action.shape[1])
+    #
+    #     return output
 
     def _optimizer(self):
         return tf.train.AdamOptimizer(learning_rate=self.lr)
